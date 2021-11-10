e02792e2cb627fe517769b99071f4da92eda29d7
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2021-11-10 17:24:36 +0800

    first commit

---
+++ js/anime_set.js
@@ -0,0 +1,74 @@
+//anime js 設定
+anime({
+  targets: ".book_animate_l",
+  top: "-35px",
+  left: " -150px",
+  translateX: 25,
+  duration: 1000,
+  easing: "easeInOutQuad",
+});
+
+// anime({
+//   targets: ".book_animate_r",
+//   top: "-130px",
+//   right: " -100px",
+//   duration: 3000,
+//   easing: "easeInOutQuad",
+// });
+
+anime({
+  targets: ".book_animate_r_1",
+  top: "-70px",
+  right: "20px",
+  duration: 2500,
+  easing: "easeInOutQuad",
+});
+
+anime({
+  targets: ".book_animate_r_2",
+  top: "26px",
+  right: "-88px",
+  duration: 3000,
+  easing: "easeInOutQuad",
+});
+
+anime({
+  targets: ".book_animate_r_3",
+  top: "-120px",
+  right: "-110px",
+  duration: 3200,
+  easing: "easeInOutQuad",
+});
+
+// anime({
+//   targets: ".book_animate_r",
+//   bottom: "40px",
+//   left: " 201px",
+//   delay: 500,
+//   easing: "easeInOutQuad",
+//   keyframes: [
+//     {
+//       delay: 1500,
+//     },
+//     {
+//       scale: 1,
+//     },
+//   ],
+// });
+
+anime({
+  targets: ".modal_5",
+  bottom: "130px",
+  left: "565px",
+  delay: 1500,
+  easing: "easeInOutQuad",
+  keyframes: [
+    {
+      scale: 1.1,
+      delay: 3500,
+    },
+    {
+      scale: 1,
+    },
+  ],
+});
